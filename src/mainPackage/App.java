a8bcfb56c069231e75ea6645f3ab9cc26338f745
Unidan <aditya.cowsik@gmail.com>
2014-04-06 17:18:50 -0500

    Just the first file, nothing special.

---
+++ src/mainPackage/App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mainPackage;
+
+/**
+ *
+ * @author Aditya
+ */
+public class App {
+    public static void main(String[] args){
+        System.out.println("Yay it works");
+        
+        
+    }
+}
